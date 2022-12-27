c6f042ba8e753622710506f56997948c0c949c18
DarkJoker360 <simoespo159@gmail.com>
2022-12-27 13:53:19 +0100

    Add initial android devices support
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- donutbattery.ps1
+++ donutbattery.ps1
@@ -15,6 +15,11 @@ function Usage {
         -v | --version : Shows donutbattery version"
 }
 
+function Detect-Device {
+    # TODO: Handle properly device detection.
+    if($(adb devices) -and ($(adb root) -or $(adb shell su -c test ls)) -eq 0) { Exit 1 }
+}
+
 function Print-Infos {
     $batt_obj | ForEach-Object {
         $model = $batt_obj.Name
@@ -52,8 +57,68 @@ function Print-Infos {
     }
 }
 
+function Print-Device-Infos {
+    $bat_dir = "/sys/class/power_supply/battery"
+    $bms_dir = "/sys/class/power_supply/bms"
+    $device_name = Write-output $(adb shell getprop ro.product.vendor.model)
+    $device_oem = Write-Output $(adb shell getprop ro.product.vendor.manufacturer)
+    Switch ($device_oem) {
+        { $_ -eq "Xiaomi" } {
+            $status = Write-Output $(adb shell cat $bat_dir/status)
+            $level = Write-Output $(adb shell cat $bat_dir/capacity)
+            $cycles = Write-Output $(adb shell cat $bms_dir/cycle_count)
+            $health = Write-Output $(adb shell cat $bat_dir/health)
+            $tech = Write-Output $(adb shell cat $bat_dir/technology)
+            $fc_cap = Write-Output $(adb shell cat $bat_dir/charge_full)
+            $d_cap = Write-Output $(adb shell cat $bat_dir/charge_full_design)
+            $v = Write-Output $(adb shell cat $bat_dir/voltage_now)
+            $battery_health =  [int] (100000 * ($fc_cap/$d_cap))
+        }
+        { $_ -eq "OnePlus" } {
+            $status = Write-Output $(adb shell cat $bat_dir/status)
+            $level = Write-Output $(adb shell cat $bat_dir/capacity)
+            $cycles = Write-Output $(adb shell cat $bat_dir/cycle_count)
+            $health = Write-Output $(adb shell cat $bat_dir/health)
+            $tech = Write-Output $(adb shell cat $bat_dir/technology)
+            $fc_cap = Write-Output $(adb shell cat $bat_dir/charge_full)
+            $d_cap = Write-Output $(adb shell cat $bat_dir/charge_full_design)
+            $v = Write-Output $(adb shell cat $bat_dir/voltage_now)
+            $battery_health =  [int] (100000 * ($fc_cap/$d_cap))
+        }
+        { $_ -eq "samsung" } {
+            $status = Write-Output $(adb shell cat $bat_dir/status)
+            $level = Write-Output $(adb shell cat $bat_dir/capacity)
+            $cycles = ""
+            $health = Write-Output $(adb shell cat $bat_dir/health)
+            $tech = Write-Output $(adb shell cat $bat_dir/technology)
+            $fc_cap = Write-Output $(adb shell cat $bat_dir/charge_full)
+            $d_cap = Write-Output $(adb shell cat $bat_dir/charge_full_design)
+            $v = Write-Output $(adb shell cat $bat_dir/voltage_now)
+            $battery_health =  [int] (100000 * ($fc_cap/$d_cap))
+        }
+        Default {
+            $status = Write-Output $(adb shell cat $bat_dir/status)
+            $level = Write-Output $(adb shell cat $bat_dir/capacity)
+        }
+    }
+
+    Write-Output "*********** $device_name ***********"
+    Write-Output "Manufacturer: $device_oem"
+    if ($status -ne "") { Write-Output "Status: $status" }
+    if ($level -ne "") { Write-Output "Level: $level%" }
+    if ($battery_health -ne "") { Write-Output "Health: $battery_health%" }
+    if ($fc_cap -ne "") { Write-Output "Full charge capacity: $fc_cap µWh" }
+    if ($d_cap -ne "") { Write-Output "Designed Capacity: $d_cap µWh" }
+    if ($v -ne "") { Write-Output "Voltage: $v µV" }
+    Write-Output "************************************"
+}
+
 Switch($args[0]) {
     { $_ -eq "-v" -or $_ -eq "--version" } { Write-Output "Donutbattery $version"}
     { $_ -eq "-h" -or $_ -eq "--help" } { Usage }
-    Default { Print-Infos }
+    Default {
+        Print-Infos
+        Detect-Device 2>&1 | Out-Null
+        Print-Device-Infos
+    }
 }
